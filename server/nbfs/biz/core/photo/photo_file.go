b2e0b062d083b00a901f6690881d2dc2217b1ba2
febrain <xdwork2015@gmail.com>
2018-07-25 11:07:29 +0800

    update

--- server/nbfs/biz/core/photo/photo_file.go
+++ server/nbfs/biz/core/photo/photo_file.go
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"github.com/disintegration/imaging"
 	"github.com/golang/glog"
-	base2 "github.com/nebulaim/telegramd/biz/base"
+	base2 "github.com/nebulaim/telegramd/server/nbfs/biz/base"
 	"github.com/nebulaim/telegramd/proto/mtproto"
 	"github.com/nebulaim/telegramd/server/nbfs/biz/dal/dataobject"
 	"image"
